14ae3725a81f5d3aaac0e71c9d4fcc264c11286a
KayleMine <60858771+KayleMine@users.noreply.github.com>
2025-10-16 14:41:48 +0300

    Update support: chache

--- MagicCore/Misc/support.lua
+++ MagicCore/Misc/support.lua
@@ -3,6 +3,7 @@ MagicCore.Support = MagicCore.Support or {}
 local Support = MagicCore.Support
 local talentCache = {}
 local isCacheBuilt = false
+local time_talents = 0
 
 MagicCore.CEnum = {
     None = 0,
@@ -22,72 +23,62 @@ MagicCore.CEnum = {
 
 local function BuildTalentCache()
     wipe(talentCache)
-    local numTabs = GetNumTalentTabs()
-    for tab = 1, numTabs do
-        local numTalents = GetNumTalents(tab)
-        for i = 1, numTalents do
-            local link = GetTalentLink(tab, i)
-            if link then
+
+    for tab = 1, GetNumTalentTabs() do
+        for idx = 1, GetNumTalents(tab) do
+            local name, _, _, _, rank = GetTalentInfo(tab, idx)
+            local link = GetTalentLink(tab, idx)
+
+            if link and name then
                 local _, _, talentId = string.find(link, "talent:(%d+)")
+                talentId = tonumber(talentId)
+
                 if talentId then
-					--print('link: ', link, " :: ", talentId)
-                    talentCache[tonumber(talentId)] = { tab = tab, index = i }
+                    talentCache[talentId] = { name = name, rank = rank, tab = tab, index = idx }
+                    talentCache[name] = { spellID = talentId, rank = rank, tab = tab, index = idx }
                 end
             end
         end
     end
+
     isCacheBuilt = true
 end
 
+
 local talentEventHandler = CreateFrame("Frame")
-talentEventHandler:RegisterEvent("PLAYER_TALENTS_CHANGED")
-talentEventHandler:SetScript("OnEvent", function(self, event, ...)
-    if event == "PLAYER_TALENTS_CHANGED" then
-        isCacheBuilt = false
-    end
+talentEventHandler:RegisterEvent("PLAYER_TALENT_UPDATE")
+talentEventHandler:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+talentEventHandler:RegisterEvent("PLAYER_ENTERING_WORLD")
+talentEventHandler:SetScript("OnEvent", function()
+	local now = GetTime()
+	-- Prevent consecutive updates in case any of the three events fire within a short period of time. -- from Morb
+	if  now-time_talents > 1 then
+		time_talents = now
+		BuildTalentCache()
+	end
 end)
 
 
-Support.iknow = function(spellID) --print(spellID)
-	if type(spellID) == 'nil' then return false end
-    if not isCacheBuilt then
-        BuildTalentCache()
-    end
-	local rank;
-    local IsSpellKnown = IsSpellKnown(spellID, isPetSpell)
-    local location = talentCache[spellID]
-	
-    if location then
-		_, _, _, _, rank = GetTalentInfo(location.tab, location.index)
-    end
+Support.iknow = function(spell)
+    if not spell then return false end
+    if not isCacheBuilt then BuildTalentCache() end
 
-    if IsSpellKnown or rank and (rank > 0) then
-        return true 
-    else 
-        return false 
-    end
+    local entry = talentCache[spell]
+    if not entry then return IsSpellKnown(spell) end
+
+    local rank = entry.rank or 0
+    return (rank > 0) or IsSpellKnown(entry.spellID or spell)
 end
 
-Support.TalentRank = function(spellID)
-	if type(spellID) == 'nil' then return false end
-    if not isCacheBuilt then
-        BuildTalentCache()
-    end
-	local rank;
-    --local IsSpellKnown = IsSpellKnown(spellID, isPetSpell)
-    local location = talentCache[spellID]
-	
-    if location then
-		_, _, _, _, rank = GetTalentInfo(location.tab, location.index)
-    end
+Support.TalentRank = function(spell)
+    if not spell then return 0 end
+    if not isCacheBuilt then BuildTalentCache() end
 
-    if rank and (rank > 0) then
-        return rank 
-    else 
-        return 0 
-    end
+    local entry = talentCache[spell]
+    if not entry then return 0 end
+
+    return entry.rank or 0
 end
-
 
 Support.toggle = function(name)
 	if _A.DSL:Get("toggle")(_,name) then
@@ -101,4 +92,14 @@ Support.IsCasting = function()
         return true
     end
     return false
+end
+
+Support.FlexItem = function(itemID, width, height, bool)
+	local itemIcon = GetItemIcon(itemID) 
+	local var = " \124T"..(itemIcon)..":"..(height or 25)..":"..(width or 25).."\124t "
+	if bool then
+		return var .. GetItemInfo(itemID)
+	else
+		return var
+	end
 end
